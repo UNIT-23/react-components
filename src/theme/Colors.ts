59cf54f1b33fa1d47880921fcd975dbe6d302ef1
Rashid Mahmood <rm_ch777@live.com>
2019-01-23 17:33:49 +0500

    Colors and Fonts refactor

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -1,16 +1,21 @@
 // tslint:disable:no-let
 // tslint:disable-next-line:readonly-keyword
 export let Theme = {
-	primary: "#00796B",
-	primaryLight: "#009688",
-	secondary: "#FFC107",
-	secondaryDark: "#FFA000",
-	secondaryLight: "#FFECB3",
-	background: "#BDBDBD",
-	profileBackground: "#BDBDBD",
-	danger: "#d32f2f",
-	warning: "#FFEB3B",
-	success: "#4CAF50",
-	text: "#212121",
-	frost: "#757575"
+	primary: "##FFFFFF",
+	primaryLight: "#FFFFFF",
+	primaryDark: "#F4F5F8",
+	secondary: "##85C733",
+	secondaryLight: "rgba(133, 199, 51, 0.12)",
+	secondaryDark: "##3D974F",
+	backgroundColor: "#F9F9F9",
+	profileBackground: "#F9F9F9",
+	accent: "#FBC01B",
+	accentLight: "#fff8ed",
+	accentDark: "##f2b900",
+	danger: "##FE2B0E",
+	gray: "#2222",
+	info: "#252631",
+	warning: "##FBC01B",
+	success: "##6DD230",
+	icons: "#778CA2"
 }
