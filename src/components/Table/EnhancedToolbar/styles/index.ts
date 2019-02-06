f6843397538c076cfb651aac81483cb2a4128f54
ahmadnoor <bc180201966@vu.edu.pk>
2019-02-06 16:49:24 +0500

    fixed EnhancedTooblar style

--- src/components/Table/EnhancedToolbar/styles/index.ts
+++ src/components/Table/EnhancedToolbar/styles/index.ts
@@ -1,37 +1,35 @@
-import { createStyles } from "@material-ui/core"
+import { createStyles, Theme } from "@material-ui/core"
 import { StyleRules } from "@material-ui/core/styles/withStyles"
-import { Theme, Fonts } from "../../../../theme"
+import { Fonts } from "../../../../theme"
 
-import { MuiTheme } from "../../../../theme/"
-
-export default (theme: typeof MuiTheme): StyleRules =>
-    createStyles({
-        root: {
-            backgroundColor: Theme.primaryLight
-        },
-        searchBarSpacer: {
-            flex: "0 1 5%"
-        },
-        searchBar: {
-            color: theme.palette.text.secondary,
-            maxWidth: 281,
-            "& input": {
-                maxHeight: 41,
-                marginTop: 0,
-                backgroundColor: Theme.gray,
-                "&::-webkit-input-placeholder": {
-                    color: "#D3D7E1"
-                }
-            }
-        },
-        actionButtonSpacer: {
-            flex: "1 1 0%"
-        },
-        title: {
-            flex: "0 0 auto",
-            fontWeight: 500,
-            color: Theme.secondary,
-            textTransform: "uppercase",
-            fontSize: Fonts.size.h5
-        }
-    })
+export default (theme: Theme): StyleRules =>
+	createStyles({
+		root: {
+			backgroundColor: theme.palette.primary.light
+		},
+		searchBarSpacer: {
+			flex: "0 1 5%"
+		},
+		searchBar: {
+			color: theme.palette.text.secondary,
+			maxWidth: 281,
+			"& input": {
+				maxHeight: 41,
+				marginTop: 0,
+				backgroundColor: theme.palette.grey["100"],
+				"&::-webkit-input-placeholder": {
+					color: "#D3D7E1"
+				}
+			}
+		},
+		actionButtonSpacer: {
+			flex: "1 1 0%"
+		},
+		title: {
+			flex: "0 0 auto",
+			fontWeight: 500,
+			color: theme.palette.secondary.main,
+			textTransform: "uppercase",
+			fontSize: Fonts.size.h5
+		}
+	})
